ba5bad1d660fcc89512ded4955e31ee11b241a20
otonye77 <oamietubodie@gmail.com>
2024-05-26 03:16:33 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export default class AppComponent implements OnInit {
   title = 'my-app';
+  users: any;
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.http.get(`http://172.20.10.12:5067/api/User`).subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed'),
+    });
+  }
 }
