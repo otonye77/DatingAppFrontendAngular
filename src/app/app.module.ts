ad6c244f5b57091ab5a15ca96a715c02ef4c12d8
otonye77 <oamietubodie@gmail.com>
2024-06-16 22:32:24 +0100

    photo gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,7 +29,6 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
     HomeComponent,
     RegisterComponent,
     MemberListComponent,
-    MemberDetailComponent,
     ListsComponent,
     MessagesComponent,
     TestErrorComponent,
